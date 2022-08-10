04e3d9f9867265573abfd8a843824fa91375aa73
pro-shiv <shivamtikkha@gmai.com>
2022-08-10 17:57:48 +0530

    first commit

---
+++ Num_printing.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a natural number :";
+    cin >> n;
+
+    if (n == 1)
+    {
+        cout << "One";
+    }
+    else if (n == 2)
+    {
+        cout << "tw0";
+    }
+    else if (n == 3)
+    {
+        cout << "three";
+    }
+    else if (n == 4)
+    {
+
+        cout << "four";
+    }
+    else if (n == 5)
+    {
+        cout << "five";
+    }
+    else if (n == 6)
+    {
+        cout << "six";
+    }
+    else if (n == 7)
+    {
+        cout << "seven";
+    }
+    else if (n == 8)
+    {
+        cout << "eight";
+    }
+    else if (n == 9)
+    {
+        cout << "nine";
+    }
+    else
+    {
+        cout << "Greater than nine";
+    }
+}
