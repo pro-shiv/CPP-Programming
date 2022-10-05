7a9a4bee6ccea2980306a8116ae81af9fc9ce1ab
pro-shiv <shivamtikkha@gmail.com>
2022-10-05 16:33:42 +0530

    initial commit

---
+++ gamewithfriend.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        if (a >= b)
+        {
+            b += c;
+        }
+        else
+        {
+            a += c;
+        }
+        if (a >= b)
+        {
+            b += d;
+        }
+        else
+        {
+            a += d;
+        }
+        if (a >= b)
+        {
+            cout << "N" << endl;
+        }
+        else
+        {
+            cout << "S" << endl;
+        }
+    }
+}
