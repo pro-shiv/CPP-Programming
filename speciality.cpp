ad87a65b9ebf818defb095562b4ac400947b94af
pro-shiv <shivamtikkha@gmail.com>
2022-10-05 20:32:20 +0530

    initial commit

---
+++ speciality.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        if (x > y && x > z)
+        {
+            cout << "Setter" << endl;
+        }
+        if (y > x && y > z)
+        {
+            cout << "Tester" << endl;
+        }
+        if (z > x && z > y)
+        {
+            cout << "Editorialist" << endl;
+        }
+    }
+}
